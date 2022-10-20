dec9f39a03caab60b234148a3f64bd0a6122ae0e
Martín Termini <martin.termini@mediamonks.com>
2022-10-20 10:39:39 +0200

    fix import reference

--- scripts/deploy-hello.ts
+++ scripts/deploy-hello.ts
@@ -1,7 +1,7 @@
 import "@nomiclabs/hardhat-ethers";
 import { Contract } from "ethers";
 import { ContractName } from "../data/enum/contract";
-import { deployContractToPrivateTestNetwork } from "../src/util/contract";
+import { deployContractToPrivateTestNetwork } from "../src/util/deploy";
 
 const deploy = async () => {
   return await deployContractToPrivateTestNetwork(ContractName.HelloWorld);
